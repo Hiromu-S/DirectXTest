16b583cdcbdfed746dc615c346d36b8abe679670
Sugawara Hiromu <homuhomu0131@gmail.com>
2019-04-17 14:59:51 +0900

    Update light.hlsl

--- HLSL/light.hlsl
+++ HLSL/light.hlsl
@@ -29,21 +29,21 @@ SamplerState SampleType : register(s0);
 PixelInputType LightVertexShader(VertexInputType input) {
 	PixelInputType output;
 
-	// Change the position vector to be 4 units for proper matrix calculations.
+	//Change the position vector to be 4 units for proper matrix calculations.
 	input.position.w = 1.0f;
 
-	// Calculate the position of the vertex against the world, view, and projection matrices.
+	//Calculate the position of the vertex against the world, view, and projection matrices.
 	output.position = mul(input.position, worldMatrix);
 	output.position = mul(output.position, viewMatrix);
 	output.position = mul(output.position, projectionMatrix);
 
-	// Store the texture coordinates for the pixel shader.
+	//Store the texture coordinates for the pixel shader.
 	output.tex = input.tex;
 
-	// Calculate the normal vector against the world matrix only.
+	//Calculate the normal vector against the world matrix only.
 	output.normal = mul(input.normal, (float3x3) worldMatrix);
 
-	// Normalize the normal vector.
+	//Normalize the normal vector.
 	output.normal = normalize(output.normal);
 
 	return output;
@@ -64,7 +64,7 @@ float4 LightPixelShader(PixelInputType input) : SV_TARGET {
 	//Invert the light direction for calculations.
 	lightDir = -lightDirection;
 
-	// Calculate the amount of light on this pixel.	
+	//Calculate the amount of light on this pixel.	
 	lightIntensity = saturate(dot(input.normal, lightDir));
 
 	if (lightIntensity > 0.0f) {
